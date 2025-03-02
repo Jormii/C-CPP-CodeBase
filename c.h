9a8888c1a9ade3991f44b7aeb5a574f1cf5aaf3e
Jorge López <lopeznatalj@gmail.com>
2025-03-02 17:32:55 +0100

    Simplified c.h and updated tests

--- c.h
+++ c.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stddef.h>
 #include <stdint.h>
 
 typedef uint8_t u8;
@@ -11,8 +12,8 @@ typedef int64_t i64;
 #define NOTE(EXPR) // Explicit comment
 
 // To indicate if a function was tested
-#define TESTED
-#define UNTESTED MUST(0 && "UNTESTED")
+#define TESTED()
+#define UNTESTED(EXPR) MUST(0 && #EXPR)
 
 #ifdef NDEBUG
 #define MUST(EXPR)    // Assert. Exits if EXPR is false
@@ -38,75 +39,16 @@ extern void must_cb(const char *expr, const char *file, i32 line);
 extern void assertz_cb(const char *expr, const char *file, i32 line);
 
 #define C_ARR_LEN(c_arr)                                                       \
+    TESTED()                                                                   \
     (((c_arr) == NULL) ? 0 : sizeof((c_arr)) / sizeof(*(c_arr)))
 
-// Results wrap a flag and a value
-#define RES(T)                                                                 \
-    typedef struct T##Res {                                                    \
-        i32 ok;                                                                \
-        T value;                                                               \
-    } T##Res;                                                                  \
-                                                                               \
-    T *T##Res_get(T##Res *res) {                                               \
-        ASSERTZ(res->ok);                                                      \
-        return &(res->value);                                                  \
-    }                                                                          \
-                                                                               \
-    const T *T##Res_getc(T##Res *res) {                                        \
-        ASSERTZ(res->ok);                                                      \
-        return &(res->value);                                                  \
-    }
-
-#define RES_C(T)                                                               \
-    typedef struct T##Res {                                                    \
-        i32 ok;                                                                \
-        const T value;                                                         \
-    } T##ResC;                                                                 \
-                                                                               \
-    const T *T##ResC_getc(T##ResC *res) {                                      \
-        ASSERTZ(res->ok);                                                      \
-        return &(res->value);                                                  \
-    }
-
-#define RES_OK(RES, val) (RES){.ok = 1, .value = val};
-#define RES_FAIL(RES, EXPR)                                                    \
-    if (!(EXPR)) {                                                             \
-        assertz_cb(#EXPR, __FILE__, __LINE__);                                 \
-        return (RES){.ok = 0};                                                 \
-    }
-
-// Buffers store a pointer and a length
-#define BUF(T)                                                                 \
-    typedef struct T##Buf {                                                    \
-        T *ptr;                                                                \
-        const i32 len;                                                         \
-    } T##Buf;                                                                  \
-                                                                               \
-    T *T##Buf_ptr(T##Buf buf, i32 idx) {                                       \
-        _BUF_ASSERT(idx, buf.ptr, buf.len)                                     \
-        return buf.ptr + idx;                                                  \
-    }                                                                          \
-                                                                               \
-    const T *T##Buf_ptrc(T##Buf buf, i32 idx) {                                \
-        _BUF_ASSERT(idx, buf.ptr, buf.len)                                     \
-        return buf.ptr + idx;                                                  \
-    }
-
-#define BUF_C(T)                                                               \
-    typedef struct T##BufC {                                                   \
-        const T *ptr;                                                          \
-        const i32 len;                                                         \
-    } T##BufC;                                                                 \
-                                                                               \
-    const T *T##BufC_ptrc(T##BufC buf, i32 idx) {                              \
-        _BUF_ASSERT(idx, buf.ptr, buf.len)                                     \
-        return buf.ptr + idx;                                                  \
-    }
-
-#define BUF_FROM_C_ARR(c_arr) {.ptr = (c_arr), .len = C_ARR_LEN(c_arr)}
-
-#define _BUF_ASSERT(idx, ptr, len)                                             \
+#define C_ARR_ASSERT(ptr, len)                                                 \
+    TESTED()                                                                   \
     ASSERTZ(ptr != NULL);                                                      \
-    ASSERTZ(len >= 0);                                                         \
+    ASSERTZ(len >= 0);
+
+#define C_ARR_IDX_ASSERT(ptr, len, idx)                                        \
+    TESTED();                                                                  \
+    C_ARR_ASSERT(ptr, len);                                                    \
     ASSERTZ(idx >= 0);                                                         \
     ASSERTZ(idx < len);
--- tests/test.c
+++ tests/test.c
@@ -1,17 +1,9 @@
 #include "c.h"
 
-RES(i32)
-RES_C(i64)
-
-BUF(i32)
-BUF_C(i64)
-
 // c.h::C_ARR_LEN
 i32 C_ARR_LEN_test(void) {
-    const i32 LEN = 3;
-
-    i32 arr[LEN];
-    ASSERTZ(C_ARR_LEN(arr) == LEN);
+    i32 arr[3];
+    ASSERTZ(C_ARR_LEN(arr) == 3);
 
     return 1;
 }
@@ -27,162 +19,66 @@ i32 C_ARR_LEN_NULL_test(void) {
 }
 
 i32 C_ARR_LEN_arg_ptr_test(void) {
-    const i32 LEN = 3;
-
-    i32 arr[LEN];
+    i32 arr[3];
     i32 *decay = arr;
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wsizeof-pointer-div"
-    ASSERTZ(C_ARR_LEN(decay) != LEN);
+    ASSERTZ(C_ARR_LEN(decay) != 3);
 #pragma clang diagnostic pop
 
     return 1;
 }
 
-// c.h::RES
-i32Res is_odd(i32 n) {
-    RES_FAIL(i32Res, (n % 2) == 1);
-    return RES_OK(i32Res, n);
-}
-
-i32 RES_get_test(void) {
-    i32Res odd_res = is_odd(1);
-    i32Res even_res = is_odd(2);
-    i32 *odd = i32Res_get(&odd_res);
-    i32 *even = i32Res_get(&even_res);
-
-    ASSERTZ(odd_res.ok);
-    ASSERTZ(odd != NULL);
-    ASSERTZ(!even_res.ok);
-    ASSERTZ(even == NULL);
-
+// c.h::C_ARR_ASSERT
+i32 C_ARR_ASSERT_call(const i32 *ptr, i32 len) {
+    C_ARR_ASSERT(ptr, len);
     return 1;
 }
 
-i32 RES_getc_test(void) {
-    i32Res ok_res = {.ok = 1};
-    i32Res fail_res = {.ok = 0};
-    const i32 *ok = i32Res_getc(&ok_res);
-    const i32 *fail = i32Res_getc(&fail_res);
+i32 C_ARR_ASSERT_ptr_is_NULL_test(void) {
+    i32 *ptr = NULL;
+    i32 len = 0;
 
-    ASSERTZ(ok_res.ok);
-    ASSERTZ(ok != NULL);
-    ASSERTZ(!fail_res.ok);
-    ASSERTZ(fail == NULL);
-
-    return 1;
-}
-// c.h::RES_C
-i32 RES_C_get_test(void) {
-    i64ResC ok_res = {.ok = 1};
-    i64ResC fail_res = {.ok = 0};
-    const i64 *ok = i64ResC_getc(&ok_res);
-    const i64 *fail = i64ResC_getc(&fail_res);
-
-    ASSERTZ(ok_res.ok);
-    ASSERTZ(ok != NULL);
-    ASSERTZ(!fail_res.ok);
-    ASSERTZ(fail == NULL);
-
-    return 1;
-}
-
-// c.h::BUF
-i32 BUF_ptr_test(void) {
-    i32 arr[] = {1, 2, 3};
-    i32Buf buf = BUF_FROM_C_ARR(arr);
-
-    for (i32 i = 0; i < buf.len; ++i) {
-        i32 *p = i32Buf_ptr(buf, i);
-        ASSERTZ(p == (arr + i));
-    }
-
-    return 1;
-}
-
-i32 BUF_ptrc_test(void) {
-    i32 arr[] = {1, 2, 3};
-    i32Buf buf = BUF_FROM_C_ARR(arr);
-
-    for (i32 i = 0; i < buf.len; ++i) {
-        const i32 *p = i32Buf_ptrc(buf, i);
-        ASSERTZ(p == (arr + i));
-    }
-
-    return 1;
-}
-
-// c.h::BUF_C
-i32 BUF_C_ptrc_test(void) {
-    i64 arr[] = {1, 2, 3};
-    i64BufC buf = BUF_FROM_C_ARR(arr);
-
-    for (i32 i = 0; i < buf.len; ++i) {
-        const i64 *p = i64BufC_ptrc(buf, i);
-        ASSERTZ(p == (arr + i));
-    }
-
-    return 1;
-}
-
-// c.h::BUF_FROM_C_ARR
-i32 BUF_FROM_C_ARR_test(void) {
-#define LEN 3
-
-    i32 arr[LEN] = {1, 2, 3};
-    i32Buf buf = BUF_FROM_C_ARR(arr);
-
-    ASSERTZ(buf.ptr == arr);
-    ASSERTZ(buf.len == LEN);
-
-    return 1;
-#undef LEN
-}
-
-i32 BUF_FROM_C_ARR_NULL_test(void) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wpointer-arith"
-#pragma clang diagnostic ignored "-Wsizeof-pointer-div"
-    i32Buf buf = BUF_FROM_C_ARR(NULL);
-#pragma clang diagnostic pop
-
-    ASSERTZ(buf.ptr == NULL);
-    ASSERTZ(buf.len == 0);
-
-    return 1;
-}
-
-// c.h::_BUF_ASSERT
-i32 _BUF_ASSERT_call(i32Buf buf, i32 idx) {
-    _BUF_ASSERT(idx, buf.ptr, buf.len);
-    return 1;
-}
-
-i32 _BUF_ASSERT_ptr_is_NULL_test(void) {
-    i32Buf buf = {.ptr = NULL, .len = 0};
-
-    i32 ok = _BUF_ASSERT_call(buf, 0);
+    i32 ok = C_ARR_ASSERT_call(ptr, len);
     ASSERTZ(!ok);
 
     return 1;
 }
 
-i32 _BUF_ASSERT_undeflow_test(void) {
+i32 C_ARR_ASSERT_len_is_negative_test(void) {
     i32 arr[] = {1, 2, 3};
-    i32Buf buf = BUF_FROM_C_ARR(arr);
+    i32 len = -1;
 
-    i32 ok = _BUF_ASSERT_call(buf, -1);
+    i32 ok = C_ARR_ASSERT_call(arr, len);
     ASSERTZ(!ok);
 
     return 1;
 }
 
-i32 _BUF_ASSERT_overflow_test(void) {
-    i32 arr[] = {1, 2, 3};
-    i32Buf buf = BUF_FROM_C_ARR(arr);
+// c.h::C_ARR_IDX_ASSERT
+i32 C_ARR_IDX_ASSERT_call(const i32 *ptr, i32 len, i32 idx) {
+    C_ARR_IDX_ASSERT(ptr, len, idx);
+    return 1;
+}
 
-    i32 ok = _BUF_ASSERT_call(buf, buf.len);
+i32 C_ARR_IDX_ASSERT_underflow_test(void) {
+    i32 arr[] = {1, 2, 3};
+    i32 len = C_ARR_LEN(arr);
+    i32 idx = -1;
+
+    i32 ok = C_ARR_IDX_ASSERT_call(arr, len, idx);
+    ASSERTZ(!ok);
+
+    return 1;
+}
+
+i32 C_ARR_IDX_ASSERT_overflow_test(void) {
+    i32 arr[] = {1, 2, 3};
+    i32 len = C_ARR_LEN(arr);
+    i32 idx = len + 1;
+
+    i32 ok = C_ARR_IDX_ASSERT_call(arr, len, idx);
     ASSERTZ(!ok);
 
     return 1;
